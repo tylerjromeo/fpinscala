8c5e6f922a510284e23be183ade21ccb452af734
Tyler Romeo <tylerjromeo@gmail.com>
2017-03-25 21:32:47 -0400

    solve 5.1

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,15 +1,19 @@
 package fpinscala.laziness
 
-import Stream._
-trait Stream[+A] {
+sealed trait Stream[+A] {
+
+  def toList: List[A] = this match {
+    case Empty => Nil: List[A]
+    case Cons(h, t) => h() :: t().toList
+  }
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -17,6 +21,7 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
+
   def take(n: Int): Stream[A] = ???
 
   def drop(n: Int): Stream[A] = ???
@@ -32,7 +37,9 @@ trait Stream[+A] {
 
   def startsWith[B](s: Stream[B]): Boolean = ???
 }
+
 case object Empty extends Stream[Nothing]
+
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
@@ -45,11 +52,27 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
+
   def from(n: Int): Stream[Int] = ???
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
+
+  def test[A, B](name: String, ioTable: Seq[(A, B)]): Unit = {
+    println(s"$name test:")
+    ioTable.foreach {
+      case (expected, result) if expected != result => println(s"expected $expected but was $result")
+      case _ => ()
+    }
+    println(s"$name test complete\n")
+  }
+
+  def main(args: Array[String]): Unit = {
+    test("toList", Seq(
+      (List(1, 2, 3), Stream(1, 2, 3).toList)
+    ))
+  }
 }
