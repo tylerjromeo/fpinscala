170487082a1914b50c3d9ffc4fec01af471ef9a7
Tyler Romeo <tylerjromeo@gmail.com>
2017-03-25 20:38:27 -0400

    solve 4.6

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,13 +4,21 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
+ def map[B](f: A => B): Either[E, B] = flatMap(e => Right(f(e)))
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+   case Left(l) => Left(l)
+   case Right(r) => f(r)
+ }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+   case Left(_) => b
+   case Right(r) => Right(r)
+ }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+   flatMap(a => b.map(f(a, _)))
+ }
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
@@ -34,4 +42,35 @@ object Either {
     try Right(a)
     catch { case e: Exception => Left(e) }
 
+  def main(args: Array[String]): Unit = {
+    test("map", Seq(
+      (Right(2), Right(1).map(_ + 1)),
+      (Left(1), Left(1).asInstanceOf[Either[Int, Int]].map(_ + 1))
+    ))
+    test("flatMap", Seq(
+      (Right(2), Right(1).flatMap(x => Right(x + 1))),
+      (Left(1), Left(1).asInstanceOf[Either[Int, Int]].flatMap(x => Right(x + 1))),
+      (Left("bap"), Right(1).flatMap(_ => Left("bap")))
+    ))
+    test("orElse", Seq(
+      (Right(1), Right(1).orElse(Left(2))),
+      (Right(2), Left(1).orElse(Right(2))),
+      (Left(2), Left(1).orElse(Left(2)))
+    ))
+    test("map2", Seq(
+      (Right(5), Right(1).map2(Right(4))(_ + _)),
+      (Left("first"), Left("first").asInstanceOf[Either[String, Int]].map2(Right(4))(_ + _)),
+      (Left("second"), Right(1).map2(Left("second").asInstanceOf[Either[String, Int]])(_ + _)),
+      (Left("first"), Left("first").asInstanceOf[Either[String, Int]].map2(Left("second").asInstanceOf[Either[String, Int]])(_ + _))
+    ))
+  }
+
+  def test[A, B](name: String, ioTable: Seq[(A, B)]): Unit = {
+    println(s"$name test:")
+    ioTable.foreach {
+      case (expected, result) if expected != result => println(s"expected $expected but was $result")
+      case _ => ()
+    }
+    println(s"$name test complete\n")
+  }
 }
