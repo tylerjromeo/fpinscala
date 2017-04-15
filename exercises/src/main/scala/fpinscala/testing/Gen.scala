000d96d40807ed4b3fdba3f4b6bc3a595bd8c418
Tyler Romeo <tylerjromeo@gmail.com>
2017-04-15 14:27:08 -0400

    solve 8.4

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,31 +1,42 @@
 package fpinscala.testing
 
+import fpinscala.state.{RNG, State}
+import fpinscala.testing.Prop.{FailedCase, SuccessCount}
+
 /*
 The library developed in this chapter goes through several iterations. This file is just the
 shell, which you can fill in and modify while working through the chapter.
 */
 
 trait Prop {
-  def check: Boolean = ???
+  def check: Either[(FailedCase, SuccessCount), SuccessCount] = ???
 
-  def &&(p: Prop): Prop = new Prop {
-    override def check: Boolean = Prop.this.check && p.check
-  }
+//  def &&(p: Prop): Prop = new Prop {
+//    override def check: Boolean = Prop.this.check && p.check
+//  }
 }
 
 object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
+case class Gen[A](sample: State[RNG, A])
+
+
 object Gen {
   def unit[A](a: => A): Gen[A] = ???
+  def choose(start: Int, stopExclusive:Int): Gen[Int] = Gen[Int](sample =
+    State[RNG, Int](RNG.map[Int, Int](RNG.nonNegativeLessThan(stopExclusive - start))(_ + start))
+  )
 }
 
-trait Gen[A] {
-  def map[A, B](f: A => B): Gen[B] = ???
-
-  def flatMap[A, B](f: A => Gen[B]): Gen[B] = ???
-}
+//trait Gen[A] {
+//  def map[A, B](f: A => B): Gen[B] = ???
+//
+//  def flatMap[A, B](f: A => Gen[B]): Gen[B] = ???
+//}
 
 trait SGen[+A] {
 
