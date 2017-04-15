3d47db9942f0ab31eddeafd3c136de552101568c
Tyler Romeo <tylerjromeo@gmail.com>
2017-04-15 14:00:11 -0400

    solve 8.3

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,19 +1,16 @@
 package fpinscala.testing
 
-import fpinscala.laziness.Stream
-import fpinscala.state._
-import fpinscala.parallelism._
-import fpinscala.parallelism.Par.Par
-import Gen._
-import Prop._
-import java.util.concurrent.{Executors,ExecutorService}
-
 /*
 The library developed in this chapter goes through several iterations. This file is just the
 shell, which you can fill in and modify while working through the chapter.
 */
 
 trait Prop {
+  def check: Boolean = ???
+
+  def &&(p: Prop): Prop = new Prop {
+    override def check: Boolean = Prop.this.check && p.check
+  }
 }
 
 object Prop {
@@ -25,8 +22,9 @@ object Gen {
 }
 
 trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+  def map[A, B](f: A => B): Gen[B] = ???
+
+  def flatMap[A, B](f: A => Gen[B]): Gen[B] = ???
 }
 
 trait SGen[+A] {
